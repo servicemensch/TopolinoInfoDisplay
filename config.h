0b241abc1fc11503cd648e0a86f1015dbb1b732b
servicemensch <github@servicemensch.info>
2025-06-25 11:30:53 +0200

    test

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 // Topolino Info Display config file
 
 // WIFI Settings
-const char* YourWIFI_SSID = "not found";
+const char* YourWIFI_SSID = "not-found";
 const char* YourWIFI_Passphrase = "unknown";
