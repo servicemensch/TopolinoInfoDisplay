47c453bf5da4f4c60ebc0b4078ad673f41dc77ca
servicemensch <66994162+servicemensch@users.noreply.github.com>
2025-06-25 21:31:36 +0200

    Update config.h

--- config/config.h
+++ config/config.h
@@ -3,3 +3,7 @@
 // WIFI Settings
 const char* YourWIFI_SSID = "SSID";
 const char* YourWIFI_Passphrase = "unknown";
+
+// SimpleApi (ioBroker) Authenticartion
+const char* YourSimpleAPI_User = "MyUSer";
+const char* YourSimpleAPI_Password = "unknown";
