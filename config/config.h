d242b46aad9797857aa0c1faf31ca6e6a77927e8
servicemensch <github@servicemensch.info>
2025-07-01 18:29:41 +0200

    config exaple

--- config/config.h
+++
@@ -1,9 +0,0 @@
-// Topolino Info Display config file
-
-// WIFI Settings
-const char* YourWIFI_SSID = "SSID";
-const char* YourWIFI_Passphrase = "unknown";
-
-// SimpleApi (ioBroker) Authenticartion
-const char* YourSimpleAPI_User = "MyUSer";
-const char* YourSimpleAPI_Password = "unknown";
---
+++ config/config_example.h
@@ -0,0 +1,9 @@
+// Topolino Info Display config file
+
+// WIFI Settings
+const char* YourWIFI_SSID = "SSID";
+const char* YourWIFI_Passphrase = "YourPassword";
+
+// SimpleApi (ioBroker) Authenticartion
+const char* YourSimpleAPI_User = "Username";
+const char* YourSimpleAPI_Password = "UnsecurePassword";
